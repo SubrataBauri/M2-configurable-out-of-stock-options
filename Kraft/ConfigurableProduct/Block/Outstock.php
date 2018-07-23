98b458849a1bbc6a6c47cb85401235b8b1609aa9
Subrata Bauri <subrata@digitalaptech.com>
2018-07-23 07:56:30 +0530

    remove $skipSaleableCheck

--- Kraft/ConfigurableProduct/Block/Outstock.php
+++ Kraft/ConfigurableProduct/Block/Outstock.php
@@ -8,11 +8,8 @@ class Outstock extends \Magento\Swatches\Block\Product\Renderer\Configurable
    public function getAllowProducts()
     {
         if (!$this->hasAllowProducts()){
-             $skipSaleableCheck = 1;
-             $products = $skipSaleableCheck ?
-             $this->getProduct()->getTypeInstance()->getUsedProducts($this->getProduct(), null) :
-                $this->getProduct()->getTypeInstance()->getSalableUsedProducts($this->getProduct(), null);
-            $this->setAllowProducts($products);
+             $products = $this->getProduct()->getTypeInstance()->getUsedProducts($this->getProduct(), null);
+             $this->setAllowProducts($products);
         }
         return $this->getData('allow_products');
     }
